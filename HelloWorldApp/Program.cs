80cb6ba8135029a114f9db302dd25016756192b6
Nimrod Maina <nimzy.maina@gmail.com>
2017-03-13 17:03:41 +0300

    - Add if statement

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,7 +6,21 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            WriteLine("Type in a string");
+            string input;
+            input = ReadLine();
+
+            if(input == "")
+            {
+                WriteLine("The input is empty");
+            } else if(input.Length < 5)
+            {
+                WriteLine("The string had Less than 5 characters");
+            } else if(input.Length <= 10)
+            {
+                WriteLine("The string had at least 5 but less than 10 characters");
+            }
+            WriteLine($"The input was {input}");
         }
     }
 }
