05e8c17aa3a379700f67259f93bc73909c7d25fc
Nimrod Maina <nimzy.maina@gmail.com>
2017-03-13 16:27:18 +0300

    - Add .Net Core Console App (HelloWorldApp)

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
